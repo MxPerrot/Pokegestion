f279098d7532e854a4661c223ad952e881cc88a0
sambazin <sambazin@etudiant.univ-rennes1.fr>
2024-02-21 10:16:30 +0100

    ajout des variables au pokemon.js

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,6 +1,15 @@
 class Pokemon{
-    constructor(){
-
+    constructor(id, form){
+        this.id = id;
+        this.nom = ; 
+        this.form = ;
+        this.type1 = ;
+        this.type2 = ;
+        this.gen = ;
+        this.att = ;
+        this.def = ;
+        this.stam = ;
+        this.lvl = ;
     }
 
     toString(){
