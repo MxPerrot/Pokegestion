a330bfd0a18c95c2ec378464b7a7fc510b05f2d1
maxperrot <maxime.perrot@etudiant.univ-rennes1.fr>
2024-03-11 13:55:16 +0100

    correction mineure : alignement

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,15 +1,15 @@
 class Pokemon{
     constructor(id, name, form, type1, type2, gen, att, def, stam, lvl){
-        this.id = id;
-        this.name = name;
-        this.form = form;
-        this.type1 = type1;
-        this.type2 = type2;
-        this.gen = gen;
-        this.att = att;
-        this.def = def;
-        this.stam = stam;
-        this.lvl = lvl;
+        this.id     = id;
+        this.name   = name;
+        this.form   = form;
+        this.type1  = type1;
+        this.type2  = type2;
+        this.gen    = gen;
+        this.att    = att;
+        this.def    = def;
+        this.stam   = stam;
+        this.lvl    = lvl;
     }
 
     toString(){
