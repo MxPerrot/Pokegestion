c7974789431acde72d1ad41e8fb36c95fff6e410
sambazin <sambazin@etudiant.univ-rennes1.fr>
2024-03-11 13:53:31 +0100

    création du constructeur

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,15 +1,15 @@
 class Pokemon{
-    constructor(id, form){
+    constructor(id, name, form, type1, type2, gen, att, def, stam, lvl){
         this.id = id;
-        this.nom = ; 
-        this.form = ;
-        this.type1 = ;
-        this.type2 = ;
-        this.gen = ;
-        this.att = ;
-        this.def = ;
-        this.stam = ;
-        this.lvl = ;
+        this.name = name;
+        this.form = form;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.gen = gen;
+        this.att = att;
+        this.def = def;
+        this.stam = stam;
+        this.lvl = lvl;
     }
 
     toString(){
