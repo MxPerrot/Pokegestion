359873065bb0864caec02904cb3f85dc48fe9544
maxperrot <maxime.perrot@etudiant.univ-rennes1.fr>
2024-03-11 14:54:08 +0100

    pokemon constructor

--- scripts/pokemon_class.js
+++ scripts/pokemon_class.js
@@ -23,18 +23,24 @@ class Pokemon {
         for (var i = 0; i < pokemon.length; i++) {
             if (pokemon[i].form == "Normal") {
 
-                let pokemon_id = pokemon[i].pokemon_id
-                let pokemon_name = pokemon[i].pokemon_name 
+                let pokemon_id = pokemon[i].pokemon_id //pokemon.js
+                let pokemon_name = pokemon[i].pokemon_name //pokemon.js
+                
+                
+                
+                let generation_number = generation[].generation_number // where ID is equal to pokemon[i].generation //generation.js
 
-                let generation_number = generation[] // where ID is equal to pokemon[i].generation //generation.js
+
+
+                let form = pokemon[i].form //pokemon.js
                 
-                let form = pokemon[i].form 
+                let type = pokemon_type //pokemon_type.js where ID is equal to pokemon[i].type1 and pokemon[i].type2
                 
-                let type = pokemon_type //pokemon_type.js
-                
-                let base_attack = pokemon[i].base_attack 
-                let base_defense = pokemon[i].base_defense 
-                let base_stamina = pokemon[i].base_stamina 
+
+
+                let base_attack = pokemon[i].base_attack //pokemon.js
+                let base_defense = pokemon[i].base_defense //pokemon.js
+                let base_stamina = pokemon[i].base_stamina //pokemon.js
                 
                 let charged_moves = charged_moves //pokemon_moves.js
                 let fast_moves = fast_moves //pokemon_moves.js
