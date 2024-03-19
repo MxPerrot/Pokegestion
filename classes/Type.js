ed3b6795a9e57b8d7bc9a46f4c62094093e35296
Samuel <Samuelbazin@outlook.fr>
2024-03-19 13:51:04 +0100

    Changement du constructeur

--- classes/Type.js
+++ classes/Type.js
@@ -3,25 +3,13 @@ class Type {
     all_types = [
     ]
 
-    test() {
-        // Create type table with type as keys and effectiveness as values from the type_effectiveness.js file
-        for (var i = 0; i < this.all_types.length; i++) {
-            this.all_types[i] = new Object();
-            for (var j = 0; j < this.all_types.length; j++) {
-                this[this.all_types[i]][this.all_types[j]] = 1.0;
+    constructor(type) {
+        if(!this.all_types.includes(type)) {
+            this.all_types[type] = new Object();
+            foreach(type_effectiveness[type], key => value) {
+                this.all_types[type][key] = value;
             }
         }
-    }
-
-    constructor(type_def, type_att , effectivness) {
-        this.type_def = type_def;
-        this.type_att = type_att;
-        this.effectivness = effectivness;
-        // Si la combinaison type_att/type_def n'est pas déjà dans all_types, on l'ajoute
-
-
-
-        
     }
 
     toString() {
