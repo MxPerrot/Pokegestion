3f3370ce54eea4eeee2455fbef27bb955eee2b31
maxperrot <maxime.perrot@etudiant.univ-rennes1.fr>
2024-03-19 14:18:58 +0100

    Correcting constructor

--- classes/Type.js
+++ classes/Type.js
@@ -4,11 +4,23 @@ class Type {
     ]
 
     constructor(type) {
-        if(!this.all_types.includes(type)) {
-            this.all_types[type] = new Object();
-            foreach(type_effectiveness[type], key => value) {
-                this.all_types[type][key] = value;
+        if(!this.all_types.includes(type)) { // Jsp si ça vérifie juste la clé, ce qui doit être le cas
+            this.all_types[type] = new Object(); 
+
+            // Avec une boucle
+            for (const key in type_effectiveness[type]) {
+                if (type_effectiveness[type].hasOwnProperty(key)) {
+                    this.all_types[type][key] = type_effectiveness[type][key];
+                }
             }
+            console.log(this.all_types[type]);
+            console.log("-----------------");
+
+            // Par assignation directe
+            this.all_types[type] = new Object(); 
+            this.all_types[type] = type_effectiveness[type];
+            console.log(this.all_types[type]);
+
         }
     }
 
