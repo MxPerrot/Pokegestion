fc80fbcfe59a5555bc8b81341aa46e17ac03c0b0
Samuel <Samuelbazin@outlook.fr>
2024-03-20 10:16:23 +0100

    Correction de import_pokemon

--- html/data/class_pokemon.js
+++ html/data/class_pokemon.js
@@ -1,5 +1,5 @@
 class Pokemon {
-    all_pokemon = [];
+    static all_pokemon = [];
 
     constructor(pokemon_id, pokemon_name, generation_number, form, type, base_attack, base_defense, base_stamina, charged_moves, fast_moves, elite_charged_moves, elite_fast_moves) {
         this.pokemon_id = pokemon_id //pokemon.js
@@ -14,15 +14,14 @@ class Pokemon {
         this.fast_moves = fast_moves //pokemon_moves.js
         this.elite_charged_moves = elite_charged_moves //pokemon_moves.js
         this.elite_fast_moves = elite_fast_moves //pokemon_moves.js
-        this.all_pokemon[this.pokemon_id] = this;
+        Pokemon.all_pokemon[pokemon_id] = this;
     }
 
     static import_pokemon() {
-
         // For every pokemon in pokemons.js
         for (var i = 0; i < pokemon.length; i++) {
             if (pokemon[i].form == "Normal") {
-
+                console.log(pokemon[i].pokemon_name);
                 // Base values
                 let pokemon_id = pokemon[i].pokemon_id //pokemon.js
                 let pokemon_name = pokemon[i].pokemon_name //pokemon.js
@@ -31,52 +30,47 @@ class Pokemon {
                 let base_defense = pokemon[i].base_defense //pokemon.js
                 let base_stamina = pokemon[i].base_stamina //pokemon.js
                 
+                
                 // DEBUG
-                console.log(generation[0].size);
-
-                for (let i = 0; i < generation[0].length; i++) {
-                    console.log(i);
-                }
                 /*
-                console.log(
-                //  const   array  index_objet    num  index
-                    generation[0]["Generation " + i][47]["id"]
-                ); */
-                // FIN DEBUG
-
-
-                /* TODO: fix generation_index search
-
-                // Finding primary keys for other tables
-                let generation_index = 0;
-
-                for (const gen in generation) {
-                    // schearching for the generation of the pokemon
-                    generation_index = generation[gen].findIndex(function () {
-                        return generation[gen].pokemon_id === pokemon[i].pokemon_id;
-                    })
-
-                    
+                for (let i = 0; i < generation[0].length; i++) {
+                    console.log(generation[0]["Generation " + i].size);
                 }
                 */
 
-
-                // console.log(generation_index); // debug
-
-                let type_index = pokemon_type.findIndex(function () {
-                    return (pokemon_type.pokemon_id === pokemon[i].pokemon_id) && (pokemon_type.form === pokemon[i].form);
-                })
-
-                let move_index = pokemon_moves.findIndex(function () {
-                    return (pokemon_moves.pokemon_id === pokemon[i].pokemon_id) && (pokemon_moves.form === pokemon[i].form);
-                })
-
-                // Importing values from other tables
-                let generation_number = generation[generation_index].generation_number
-                let type = pokemon_type[type_index].type_name
-                for(i = 0; i < type.length; i++) {
-                    Type(type[i]);
+                // Solution pour trouver la generation
+                let generation_num = 0;
+                for (const gen in generation[0]) {
+                    for(let u = 0; u < generation[0][gen].length; u++){
+                        if(generation[0][gen][u].id === pokemon[i].pokemon_id){
+                            generation_num = generation[0][gen][u].generation_number;
+                        }
+                    }
                 }
+                
+                let typepk
+                for(let u = 0; u < pokemon_type.length; u++){
+                    if(pokemon_type[u].pokemon_id === pokemon[i].pokemon_id && pokemon_type[u].form === pokemon[i].form){
+                        console.log(pokemon_type[u].type);
+                        typepk = pokemon_type[u].type;
+                    }
+                }
+                
+                let move_index = 0;
+                for(let u = 0; u < pokemon_moves.length; u++){
+                    if(pokemon_moves[u].pokemon_id === pokemon[i].pokemon_id && pokemon_moves[u].form === pokemon[i].form){
+                        console.log(pokemon_moves[u].charged_moves);
+                        move_index = u;
+                    }
+                }
+                // Importing values from other tables
+                let generation_number = generation_num
+                let type = typepk
+                for(let y = 0; y < type.length; y++) {
+                    console.log(type[y]);
+                    new Type(type[y]);
+                }
+
                 let charged_moves = pokemon_moves[move_index].charged_moves
                 let fast_moves = pokemon_moves[move_index].fast_moves
                 let elite_charged_moves = pokemon_moves[move_index].elite_charged_moves
@@ -114,6 +108,16 @@ class Pokemon {
             + "Elite Fast Moves: " + this.display_array(this.elite_fast_moves) + "\n";
         */
         let string = this.pokemon_id + " | " + this.pokemon_name + " | Gen " + this.generation_number + " | " + this.form + " form\n"; 
+        return string;
+    }
+
+    static toStringAllPokemon(){
+        let string = "";
+        for (const key in this.all_pokemon) {
+            if (this.all_pokemon.hasOwnProperty(key)) {
+                string += this.all_pokemon[key].toString();
+            }
+        }
         return string;
     }
 }
