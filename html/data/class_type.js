763fd3363e2bed50521901006e1182d0e0054d32
Samuel <Samuelbazin@outlook.fr>
2024-03-20 10:16:34 +0100

    correction class_type

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -1,30 +1,29 @@
 class Type {
 
-    all_types = [
+    static all_types = [
     ]
 
     constructor(type) {
-        if(!this.all_types.includes(type)) { // Jsp si ça vérifie juste la clé, ce qui doit être le cas
-            this.all_types[type] = new Object(); 
-
+           if(Type.all_types[type] == undefined) {
+            Type.all_types[type] = new Object();
             // Avec une boucle
-            for (const key in type_effectiveness[type]) {
-                if (type_effectiveness[type].hasOwnProperty(key)) {
-                    this.all_types[type][key] = type_effectiveness[type][key];
+            for (const key in type_effectiveness[0][type]) {
+                if (type_effectiveness[0][type].hasOwnProperty(key)) {
+                    Type.all_types[type][key] = type_effectiveness[0][type][key];
                 }
             }
-            console.log(this.all_types[type]);
+            console.log(Type.all_types[type]);
             console.log("-----------------");
-
+            /*
             // Par assignation directe
-            this.all_types[type] = new Object(); 
-            this.all_types[type] = type_effectiveness[type];
-            console.log(this.all_types[type]);
-
+            this.all_types[type[i]] = new Object(); 
+            this.all_types[type[i]] = type_effectiveness[0][type[i]];
+            console.log(this.all_types[type[i]]);
+            */
         }
     }
 
     toString() {
-        return this.type_name;
+        return this.all_types;
     }
 }
