be267333082c74cb9f5fecab45a6d8d9a555cd9d
Samuel <Samuelbazin@outlook.fr>
2024-03-24 15:00:22 +0100

    static toString fonctionnel

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -4,30 +4,27 @@ class Type {
     ]
 
     constructor(type) {
-           if(Type.all_types[type] == undefined) {
+        if(Type.all_types[type] == undefined) {
             Type.all_types[type] = new Object();
             // Avec une boucle
             for (const key in type_effectiveness[0][type]) {
                 if (type_effectiveness[0][type].hasOwnProperty(key)) {
-                    this.name = type;
-                    this.effectiveness = type_effectiveness[0][type];
+                    
                     Type.all_types[type][key] = type_effectiveness[0][type][key];
                 }
             }
-            console.log(Type.all_types[type]);
+            //console.log(Type.all_types[type]);
         }
     }
 
-    // méthode toString pour afficher tout les types et leur efficacité de all_types à faire.
-    toString() {
-        return ;
-    }
 
     static toString(){
+        let string = "";
         for (const key in this.all_types) {
             if (this.all_types.hasOwnProperty(key)) {
-                console.log(this.all_types[key]);
+                string += key + " ";
             }
         }
+        return string;
     }
 }
