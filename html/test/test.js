cc7e29d1cdfe0a474826f45367f29db278bf20f7
Samuel <Samuelbazin@outlook.fr>
2024-03-24 16:21:33 +0100

    ajout de test

--- html/test/test.js
+++ html/test/test.js
@@ -1,31 +1,48 @@
 Pokemon.import_pokemon();
 
-document.getElementById("test").innerText = Type.toString();
-/*
+
 function getPokemonsByType(typeName) {
     let pokemons = [];
-    for (let i = 0; i < Pokemon.all_pokemon.length; i++) {
-        // Cannot read properties of undefined (reading 'type')
-
-        if (Pokemon.getPokemonType(i).includes(typeName)) {
-            pokemons.push(Pokemon.all_pokemon[i]);
+    for(const key in Pokemon.all_pokemon) {
+        if(Pokemon.all_pokemon[key].getType().includes(typeName)) {
+            pokemons += Pokemon.all_pokemon[key] + "\n";
         }
     }
     return pokemons;
-}
-
-function retourneGetPokemonByType() {
-    result1.innerText = getPokemonsByType("Grass");
-    return 0;
 }
 
 function getPokemonsByAttack(attackName)    {
     let pokemons = [];
-    for (let i = 0; i < Pokemon.all_pokemon.length; i++) {
-        if (Pokemon.all_pokemon[i].charged_moves.includes(attackName) || Pokemon.all_pokemon[i].fast_moves.includes(attackName)) {
-            pokemons.push(Pokemon.all_pokemon[i]);
+    for(const key in Pokemon.all_pokemon) {
+        if(Pokemon.all_pokemon[key].getAllMoves().includes(attackName)) {
+            pokemons += Pokemon.all_pokemon[key] + "\n";
         }
     }
     return pokemons;
 }
-*/
+
+function getAttacksByType(typeName) {
+    let attacks = [];
+    for(const key in Attack.all_attacks) {
+        if(Attack.all_attacks[key].getType == typeName) {
+            attacks += Attack.all_attacks[key] + "\n";
+        }
+    }
+    return attacks;
+}
+
+function sortPokemonByName(){
+    Pokemon.all_pokemon.sort((a, b) => (a.getPokemonName() > b.getPokemonName()) ? 1 : -1);
+}
+
+function sortPokemonByStamina(){
+    Pokemon.all_pokemon.sort((a, b) => (a.getBaseStamina() > b.getBaseStamina()) ? 1 : -1);
+}
+
+// cherche les pokemons dont le type de l'attaque est le plus efficace
+getWeakestEnemies(attack){
+    
+}
+
+document.getElementById("test1").innerText = getPokemonsByType("Grass");
+document.getElementById("test2").innerText = getPokemonsByAttack("Thunder Shock");
