f79ae09f4d048c873414d2b1f7163e6d48626756
Samuel <Samuelbazin@outlook.fr>
2024-03-24 15:54:40 +0100

    Modification du constructeur et des toString()

--- html/data/class_type.js
+++ html/data/class_type.js
@@ -5,24 +5,39 @@ class Type {
 
     constructor(type) {
         if(Type.all_types[type] == undefined) {
-            Type.all_types[type] = new Object();
+            this.type = type;
             // Avec une boucle
             for (const key in type_effectiveness[0][type]) {
                 if (type_effectiveness[0][type].hasOwnProperty(key)) {
-                    
-                    Type.all_types[type][key] = type_effectiveness[0][type][key];
+                    this.type_effectiveness = type_effectiveness[0][type];
                 }
             }
+            Type.all_types[type] = this;
             //console.log(Type.all_types[type]);
         }
     }
 
+    displayArray() {
+        let string = "";
+        for (const key in this.type_effectiveness) {
+            if (this.type_effectiveness.hasOwnProperty(key)) {
+                string += `${key} | Effectivness ${this.type_effectiveness[key]} \n`;
+            }
+        }
+        return string;
+    }
+
+    toString() {
+        let string = "";
+        string += `Type : ${this.type} | Effectivness : {\n ${this.displayArray()}} \n`;
+        return string;
+    }
 
     static toString(){
         let string = "";
         for (const key in this.all_types) {
             if (this.all_types.hasOwnProperty(key)) {
-                string += key + " ";
+                string += this.all_types[key].toString();
             }
         }
         return string;
